77a4781e52c5b150986e5cf6ea5ec75ee6a13a87
Pushpal Sidhu <psidhu.devel@gmail.com>
2017-12-08 08:52:41 -0800

    erc: add SASL support
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

--- lisp/0015-erc.el
+++ lisp/0015-erc.el
@@ -28,5 +28,30 @@
  )
 
 ;; SASL support
+(require 'erc-sasl)
+(add-to-list 'erc-sasl-server-regexp-list "irc\\.freenode\\.net")
+
+;; https://github.com/john2x/emacs.d/blob/master/init.org
+(defun erc-login ()
+  "Perform user authentication at the IRC server. (PATCHED)"
+  (erc-log (format "login: nick: %s, user: %s %s %s :%s"
+		   (erc-current-nick)
+		   (user-login-name)
+		   (or erc-system-name (system-name))
+		   erc-session-server
+		   erc-session-user-full-name))
+  (if erc-session-password
+      (erc-server-send (format "PASS %s" erc-session-password))
+    (message "Logging in without password"))
+  (when (and (featurep 'erc-sasl) (erc-sasl-use-sasl-p))
+    (erc-server-send "CAP REQ :sasl"))
+  (erc-server-send (format "NICK %s" (erc-current-nick)))
+  (erc-server-send
+   (format "USER %s %s %s :%s"
+	   ;; hacked - S.B.
+	   (if erc-anonymous-login erc-email-userid (user-login-name))
+	   "0" "*"
+	   erc-session-user-full-name))
+  (erc-update-mode-line))
 
 (provide '0015-erc.el)
