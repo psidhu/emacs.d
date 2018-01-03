2a5b8d882acacf08d315c3a97af3c27a6e773d1d
Pushpal Sidhu <psidhu.devel@gmail.com>
2018-01-03 09:13:47 -0800

    lisp: add string-inflection
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

---
+++ lisp/0162-string-inflection.el
@@ -0,0 +1,18 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Configure string-inflection                                               ;;
+;;                                                                           ;;
+;; Author: Pushpal Sidhu <psidhu.devel@gmail.com                             ;;
+;; Date: 20180103                                                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Install it
+(install-pkg 'string-inflection)
+
+;; Use it
+(require 'string-inflection)
+
+;; Configure it
+(after-load 'string-inflection
+  (global-set-key (kbd "C-c s i") 'string-inflection-all-cycle)
+  (setq string-inflection-skip-backward-when-done t) ; Don't move point
+  )
