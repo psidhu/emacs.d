ea323cd4a89bee2c256fb858373b5b9af0184d35
Pushpal Sidhu <psidhu.devel@gmail.com>
2018-01-22 09:29:52 -0800

    lisp: alert: initial add
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

---
+++ lisp/0117-alert.el
@@ -0,0 +1,12 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Use alert package                                                         ;;
+;;                                                                           ;;
+;; Author: Pushpal Sidhu <psidhu.devel@gmail.com                             ;;
+;; Date: 20180122                                                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Install it
+(install-pkg 'alert)
+
+;; Configure it
+(setq alert-default-style 'libnotify)
