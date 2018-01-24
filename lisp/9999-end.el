d31eac9ef5178b66dffb47b4c532fa548fe7b310
Pushpal Sidhu <psidhu.devel@gmail.com>
2018-01-24 13:26:28 -0800

    lisp: end: add message when done loading lisp dir
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

---
+++ lisp/9999-end.el
@@ -0,0 +1,8 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Message that lisp files were all loaded in this directory                 ;;
+;;                                                                           ;;
+;; Author: Pushpal Sidhu <psidhu.devel@gmail.com                             ;;
+;; Date: 20180124                                                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(message (concat "Done load files in " lisp-d))
