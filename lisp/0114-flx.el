465c4bce28ba02e6599ae1356d6d75f7672d48c7
Pushpal Sidhu <psidhu.devel@gmail.com>
2018-02-09 09:35:58 -0800

    lisp: add flx
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

---
+++ lisp/0114-flx.el
@@ -0,0 +1,17 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Utilize ido                                                               ;;
+;;                                                                           ;;
+;; Author: Pushpal Sidhu <psidhu.devel@gmail.com                             ;;
+;; Date: 20180209                                                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Install it
+(install-pkg 'flx)
+
+;; Use it
+
+;; Use flx isearch and replace built-in regex searching
+(when (try-install-pkg 'flx-isearch)
+  (global-set-key (kbd "C-M-s") #'flx-isearch-forward)
+  (global-set-key (kbd "C-M-r") #'flx-isearch-backward)
+  )
