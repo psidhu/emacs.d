f05c834aee2fdfc4998b2a9ebd98d9da518acdb5
Pushpal Sidhu <psidhu.devel@gmail.com>
2019-06-12 12:41:13 -0700

    lisp: gcmh: initial add
    Add gcmh for better start times of the gc.
    
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

---
+++ lisp/0109-gcmh.el
@@ -0,0 +1,18 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; GCMH                                                                      ;;
+;;                                                                           ;;
+;; Author: Pushpal Sidhu <psidhu.devel@gmail.com                             ;;
+;; Date: 20190503                                                            ;;
+;; Notes: This is Koral's garbage collector magic hack                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Install it
+(install-pkg 'gcmh)
+
+;; Use it
+(require 'gcmh)
+(gcmh-mode t)
+
+;; Set limits
+(setq gcmh-low-cons-threshold (* 20 (* 1024 1024)))   ;Set it to 20mb
+(setq gcmh-high-cons-threshold (* 256 (* 1024 1024))) ;Set it to 256mb
