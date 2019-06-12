a1a0d33b86c5a27c7e645ae9b7224bc1f030eea3
Pushpal Sidhu <psidhu.devel@gmail.com>
2019-06-12 12:39:17 -0700

    lisp: markdown: update list of files to enable
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

--- lisp/0124-markdown.el
+++ lisp/0124-markdown.el
@@ -9,4 +9,13 @@
 (install-pkg 'markdown-mode)
 
 ;; Use it
-(require 'markdown-mode)
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+(set 'markdown-open-command 'grip)
