629e394099610de619498daf699a3e0073aac97e
Pushpal Sidhu <psidhu.devel@gmail.com>
2019-08-21 09:18:26 -0700

    undo-tree: move loading of undo-tree to near end of list
    There's some kind of bug when adding new packages, undo-tree freaks
    out. Loading at the end fixes it.
    
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

--- lisp/0996-undo-tree.el
+++ lisp/0996-undo-tree.el
@@ -1,8 +1,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Customize undo-tree                                                       ;;
 ;;                                                                           ;;
-;; Author: Pushpal Sidhu <psidhu.devel@gmail.com                             ;;
+;; Author: Pushpal Sidhu <psidhu.devel@gmail.com>                            ;;
 ;; Date: 20171130                                                            ;;
+;; Notes: Load near end of package installs due to undo-tree bug             ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Install it
@@ -15,8 +16,8 @@
 (global-undo-tree-mode)			; Replace emacs undo
 
 ;; Customize it
-(after-load 'undo-tree
-  (setq undo-tree-history-directory-alist (list `("" . ,undo-tree-d)))
-  (setq-default undo-tree-auto-save-history t
-		undo-tree-visualizer-diff t)
-  )
+;; (after-load 'undo-tree
+;;   (setq undo-tree-history-directory-alist (list `("" . ,undo-tree-d)))
+;;   (setq-default undo-tree-auto-save-history t
+;;   		undo-tree-visualizer-diff t)
+;;   )
