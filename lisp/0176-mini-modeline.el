03afd5fbabf977b40066ed2876d5aac61ef2a582
Pushpal Sidhu <psidhu.devel@gmail.com>
2019-08-21 09:19:17 -0700

    mini-modeline: Initial add
    Add installing and using the mini-modeline package.
    
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

---
+++ lisp/0176-mini-modeline.el
@@ -0,0 +1,12 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Configure mini-modeline                                                   ;;
+;;                                                                           ;;
+;; Author: Pushpal Sidhu <psidhu.devel@gmail.com>                            ;;
+;; Date: 20190821                                                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Install it
+(install-pkg 'mini-modeline)
+
+;; Enable it
+(mini-modeline-mode)
