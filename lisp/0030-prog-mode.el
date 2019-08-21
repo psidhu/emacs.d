d7dc3a767cb1edb7b199b691f190f646ab679f65
Pushpal Sidhu <psidhu.devel@gmail.com>
2019-08-21 10:17:46 -0700

    prog-mode: Remove settings
    Remove customization for now. It was causing more problems than it
    solved.
    
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

--- lisp/0030-prog-mode.el
+++ lisp/0030-prog-mode.el
@@ -4,17 +4,3 @@
 ;; Author: Pushpal Sidhu <psidhu.devel@gmail.com                             ;;
 ;; Date: 20171130                                                            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Configure compile window
-(setq compilation-window-height 10)
-
-(defun my-compilation-hook ()
-  (when (not (get-buffer-window "*compilation*"))
-    (save-selected-window
-      (save-excursion
-        (let* ((w (split-window-vertically))
-               (h (window-height w)))
-          (select-window w)
-          (switch-to-buffer "*compilation*")
-          (shrink-window (- h compilation-window-height)))))))
-(add-hook 'compilation-mode-hook 'my-compilation-hook)
