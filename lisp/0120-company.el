3a0b328fdce6bf951f4ff44a43266b7662f1f1c8
Pushpal Sidhu <psidhu.devel@gmail.com>
2020-02-06 15:25:06 -0800

    lisp: company: revamp a bit
    Revamp this file. Main issue was the wrong backend kept getting
    initialized in various major modes. No idea why. Force some major
    modes I care about.
    
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

--- lisp/0120-company.el
+++ lisp/0120-company.el
@@ -13,37 +13,22 @@
 (add-hook 'after-init-hook 'global-company-mode)
 
 ;; Configure keys
-(after-load
- 'company
- (define-key company-mode-map (kbd "M-/") 'company-complete)
- (define-key company-active-map (kbd "M-/") 'company-select-next)
- (define-key company-active-map (kbd "C-n") 'company-select-next)
- (define-key company-active-map (kbd "C-p") 'company-select-previous)
- (setq-default company-dabbrev-other-buffers 'all
-	       company-tooltip-align-annotations t
-	       company-idle-delay 0
-	       company-begin-commands '(self-insert-command)
-	       )
+(after-load 'company
+  (define-key company-mode-map (kbd "M-/") 'company-complete)
+  (define-key company-active-map (kbd "M-/") 'company-select-next)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (setq-default company-dabbrev-other-buffers 'all
+		company-tooltip-align-annotations t
+		company-idle-delay 0
+		company-begin-commands '(self-insert-command)
+		)
 
- ;; add built-in backends
- (add-to-list 'company-backends 'company-yasnippet)
- (add-to-list 'company-backends 'company-ispell)
- (add-to-list 'company-backends 'company-gtags)
- )
-
-;; Use company-irony
-(when (try-install-pkg 'company-irony)
-  (after-load
-      'company
-    (add-to-list 'company-backends 'company-irony)
-    )
-  )
-
-(when (try-install-pkg 'company-irony-c-headers)
-  (after-load
-      'company
-    (add-to-list 'company-backends 'company-irony-c-headers)
-    )
+  ;; Hard set default company
+  (setq company-backends
+	'((company-files company-keywords company-capf company-yasnippet)
+	  (company-abbrev company-dabbrev)
+	  ))
   )
 
 ;; Use company-flx
@@ -57,57 +42,88 @@
   (add-hook 'after-init-hook 'company-quickhelp-mode)
   )
 
-(when (try-install-pkg 'company-auctex) ; auctex
-  (after-load
-   'company
-   (company-auctex-init)
-   )
+(when (try-install-pkg 'company-statistics) ; statistics
+  (after-load 'company
+    (setq company-statistics-file company-stat-f)
+    (add-hook 'after-init-hook 'company-statistics-mode)
+    )
   )
 
-(when (try-install-pkg 'company-c-headers) ; c-headers
-  (after-load
-   'company
-   (add-to-list 'company-backends 'company-c-headers)
-   )
+;; Backends
+;;
+;; Company backends are kind of dumb, IMO. For example,
+;; company-anaconda won't automatically load, even if it's in the
+;; list, but there's something (e.g. company-clang) in there as well,
+;; even if it comes first in the list. I'm just going to force each
+;; backend per mode as req'd
+
+;; Use company-irony
+(when (try-install-pkg 'company-irony)
+  (when (try-install-pkg 'company-irony-c-headers)
+    (after-load 'company
+      (add-hook 'irony-mode-hook
+		(lambda ()
+		  (set (make-local-variable 'company-backends)
+		       '((company-irony-c-headers company-irony
+			  company-yasnippet company-keywords)))))
+      )
+    )
+  )
+
+(when (try-install-pkg 'company-auctex) ; auctex
+  (after-load 'company
+    (add-hook 'tex-mode-hook
+	      (lambda ()
+		(company-auctex-init)))
+    (add-hook 'latex-mode-hook
+	      (lambda ()
+		(company-auctex-init)))
+    (when (try-install-pkg 'company-math)   ; math
+      (after-load 'company
+	(add-to-list 'company-backends 'company-math-symbols-unicode)
+	)
+      )
+    )
   )
 
 (when (try-install-pkg 'company-lua)    ; lua
-  (after-load
-   'company
-   (add-to-list 'company-backends 'company-lua)
-   )
-  )
-
-(when (try-install-pkg 'company-math)   ; math
-  (after-load
-   'company
-   (add-to-list 'company-backends 'company-math-symbols-unicode)
-   )
+  (after-load 'company
+    (add-hook 'irony-mode-hook
+	      (lambda ()
+		(set (make-local-variable 'company-backends)
+		     '((company-lua company-yasnippet company-keywords)))))
+    )
   )
 
 (when (try-install-pkg 'company-shell)  ; shell
-  (after-load
-   'company
-   (add-to-list 'company-backends 'company-shell)
-   )
+  (after-load 'company
+    (add-hook 'sh-mode-hook
+	      (lambda ()
+		(set (make-local-variable 'company-backends)
+		     '(company-shell company-shell-env
+		       company-yasnippet company-keywords))))
+    (add-hook 'shell-mode-hook
+	      (lambda ()
+		(set (make-local-variable 'company-backends)
+		     '((company-shell company-shell-env
+		        company-yasnippet company-keywords)))))
+    )
   )
 
-(when (try-install-pkg 'company-statistics) ; statistics
-  (after-load
-   'company
-   (setq company-statistics-file company-stat-f)
-   (add-hook 'after-init-hook 'company-statistics-mode)
-   )
+(when (try-install-pkg 'company-anaconda)  ; anaconda
+  (after-load 'company
+    (add-hook 'python-mode-hook
+	      (lambda ()
+		(set (make-local-variable 'company-backends)
+		     '((company-anaconda company-yasnippet company-keywords)))))
+    )
   )
 
 (when (try-install-pkg 'company-web)    ; web
-  (after-load
-      'company
-    (add-hook
-     'web-mode-hook
-     (lambda ()
-       (set (make-local-variable 'company-backends) '(company-web-html))
-       )
-     )
+  (after-load 'company
+    (add-hook 'web-mode-hook
+	      (lambda ()
+		(set (make-local-variable 'company-backends)
+		     '((company-web-html company-yasnippet company-keywords)))))
     )
   )
