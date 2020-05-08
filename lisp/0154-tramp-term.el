3064825f9f08a783d2b1af08068a2eb264b9f139
Pushpal Sidhu <psidhu.devel@gmail.com>
2020-05-08 13:55:05 -0700

    lisp: tramp-term: Add shortcut for sudo open in tramp
    Beats trying to remember syntax etc.
    
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

--- lisp/0154-tramp-term.el
+++ lisp/0154-tramp-term.el
@@ -7,3 +7,13 @@
 
 ;; Install it
 (install-pkg 'tramp-term)
+
+;; Custom config
+;; https://emacs.stackexchange.com/questions/17725/how-to-sudo-save-file-in-tramp-mode
+(defun tramp-sudo-find-file (file)
+    "Opens repote FILE with root privileges."
+    (interactive "FFind file: ")
+    (setq begin (replace-regexp-in-string  "scp" "ssh" (car (split-string file ":/"))))
+    (setq end (car (cdr (split-string file "@"))))
+    (set-buffer
+     (find-file (format "%s" (concat begin "|sudo:root@" end)))))
