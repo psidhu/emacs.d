6e0a5724a61017b54a67fbfb2f2b5c5935e21829
Pushpal Sidhu <psidhu.devel@gmail.com>
2020-06-17 18:18:25 -0700

    lisp: markdown: use util/hook-ext-to-mode to completeness
    util/hook-ext-to-mode is a fn I wrote, but forgot to use here. Use it.
    
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

--- lisp/0124-markdown.el
+++ lisp/0124-markdown.el
@@ -11,8 +11,8 @@
 ;; Use it
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(util/hook-ext-to-mode "markdown" 'markdown-mode)
+(util/hook-ext-to-mode "md" 'markdown-mode)
 
 (autoload 'gfm-mode "markdown-mode"
    "Major mode for editing GitHub Flavored Markdown files" t)
