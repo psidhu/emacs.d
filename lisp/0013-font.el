e311c89e5dd281c00c1a9d209c9d0d9ee72d3a5c
Pushpal Sidhu <psidhu.devel@gmail.com>
2022-08-16 22:30:30 -0700

    lisp: font: add new config for custom fonts
    Use "Hack" when available.
    
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

---
+++ lisp/0013-font.el
@@ -0,0 +1,11 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Customize font                                                            ;;
+;;                                                                           ;;
+;; Author: Pushpal Sidhu <psidhu.devel@gmail.com>                            ;;
+;; Date: 20220805                                                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Add it
+(add-to-list 'default-frame-alist '(font . "Hack"))
+(set-face-attribute 'default nil
+		    :family "Hack")
