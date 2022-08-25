06a34ee9e04be41171f455169257e8aa6b616c4c
Pushpal Sidhu <psidhu.devel@gmail.com>
2022-08-25 10:57:29 -0700

    lisp: textsize: initial add
    Add this package for now.
    
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

---
+++ lisp/0016-textsize.el
@@ -0,0 +1,12 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Configure textsize                                                        ;;
+;;                                                                           ;;
+;; Author: Pushpal Sidhu <psidhu.devel@gmail.com>                            ;;
+;; Date: 20220805                                                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Install it
+(install-pkg 'textsize)
+
+;; Use it
+(textsize-mode t)
