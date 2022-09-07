7b7736950deb1d62d4668633f212972b4f4c0bcb
Pushpal Sidhu <psidhu.devel@gmail.com>
2022-09-07 09:26:52 -0700

    lisp: golden-ratio: Initial add
    Add this for now. Been using it for a bit and I think I like it.
    
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

---
+++ lisp/0235-golden-ratio.el
@@ -0,0 +1,15 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Configure golden-ratio                                                    ;;
+;;                                                                           ;;
+;; Author: Pushpal Sidhu <psidhu.devel@gmail.com>                            ;;
+;; Date: 20220814                                                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Install it
+(install-pkg 'golden-ratio)
+
+;; Use it
+(golden-ratio-mode t)
+
+;; Configure it
+(setq golden-ratio-auto-scale t)
