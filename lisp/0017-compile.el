05ec2a0c52ea174873f0c379548786eaaaf18a91
Pushpal Sidhu <psidhu.devel@gmail.com>
2023-07-28 11:07:52 -0700

    compile: update how compilation buffer is closed + don't close grep
    Only bury *compilation* windows
    
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

--- lisp/0017-compile.el
+++ lisp/0017-compile.el
@@ -9,33 +9,48 @@
 ;;(setq compilation-scroll-output 'first-error)
 (setq compilation-window-height 10)
 
-;; https://stackoverflow.com/questions/9725015/how-do-i-make-the-compilation-window-in-emacs-to-always-be-a-certain-size
-(defun my-compilation-hook ()
-  (when (not (get-buffer-window "*compilation*"))
-    (save-selected-window
-      (save-excursion
-        (let* ((w (split-window-vertically))
-               (h (window-height w)))
-          (select-window w)
-          (switch-to-buffer "*compilation*")
-          (shrink-window (- h compilation-window-height)))))))
-(add-hook 'compilation-mode-hook 'my-compilation-hook)
-
 ;; https://stackoverflow.com/questions/11043004/emacs-compile-buffer-auto-close
-(defun bury-compile-buffer-if-successful (buffer string)
- "Bury a compilation buffer if succeeded without warnings "
- (when (and
-         (buffer-live-p buffer)
-         (string-match "compilation" (buffer-name buffer))
-         (string-match "finished" string)
-         (not
-          (with-current-buffer buffer
-            (goto-char (point-min))
-            (search-forward "warning" nil t))))
-    (run-with-timer 1 nil
-                    (lambda (buf)
-                      (bury-buffer buf)
-		      (delete-windows-on buf))
-                      ;; (switch-to-prev-buffer (get-buffer-window buf) 'kill))
-                    buffer)))
-(add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
+(defun hide-compile-buffer-if-successful (buffer string)
+  (unless
+      (string= (buffer-name buffer) "*compilation*")
+    (error "not compilation buffer, won't auto-close")
+    )
+
+  (setq compilation-total-time (time-subtract nil compilation-start-time))
+  (setq time-str (concat " (Time: " (format-time-string "%s.%3N" compilation-total-time) "s)"))
+
+  (if
+      (with-current-buffer buffer
+        (setq warnings (eval compilation-num-warnings-found))
+        (setq warnings-str (concat " (Warnings: " (number-to-string warnings) ")"))
+        (setq errors (eval compilation-num-errors-found))
+
+        (if (eq errors 0) nil t)
+	)
+
+      ;;If Errors then
+      (message (concat "Compiled with Errors" warnings-str time-str))
+
+    ;;If Compiled Successfully or with Warnings then
+    (progn
+      (bury-buffer buffer)
+      (run-with-timer auto-hide-compile-buffer-delay nil 'delete-window (get-buffer-window buffer 'visible))
+      (message (concat "Compiled Successfully" warnings-str time-str))
+      )
+    )
+  )
+
+(make-variable-buffer-local 'compilation-start-time)
+
+(defun compilation-started (proc)
+  (setq compilation-start-time (current-time))
+  )
+
+(defcustom auto-hide-compile-buffer-delay 2
+  "Time in seconds before auto hiding compile buffer."
+  :group 'compilation
+  :type 'number
+  )
+
+(add-hook 'compilation-start-hook 'compilation-started)
+(add-hook 'compilation-finish-functions 'hide-compile-buffer-if-successful)
