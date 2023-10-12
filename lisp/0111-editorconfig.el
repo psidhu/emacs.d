33068aa1dcfb7945b1d6618c95db091e415393f5
Pushpal Sidhu <psidhu.devel@gmail.com>
2023-10-11 17:13:17 -0700

    lisp: editorconfig: add base package
    Add this for package dependencies, like dash
    
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

---
+++ lisp/0111-editorconfig.el
@@ -0,0 +1,9 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Configure editorconfig package                                            ;;
+;;                                                                           ;;
+;; Author: Pushpal Sidhu <psidhu.devel@gmail.com>                            ;;
+;; Date: 20231007                                                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Install it
+(install-pkg 'editorconfig)
