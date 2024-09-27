6948c23c79d11c74749391a433e275f263f1009a
Pushpal Sidhu <psidhu.devel@gmail.com>
2024-09-26 23:16:04 -0700

    compile: change how my compile window works
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

--- lisp/0017-compile.el
+++ lisp/0017-compile.el
@@ -9,52 +9,57 @@
 ;;(setq compilation-scroll-output 'first-error)
 (setq compilation-window-height 10)
 
-(defvar my/auto-hide-compile-buffer-delay 2)
-(defvar my/compilation-exit-code 0)
-
-(defun my/compilation-exit-message-function (status_ code msg)
-  (setq my/compilation-exit-code code)
-  (cons msg code))
-
-;; https://stackoverflow.com/questions/11043004/emacs-compile-buffer-auto-close
-(defun my/hide-compile-buffer-if-successful (buffer string)
-  (unless
-      (string= (buffer-name buffer) "*compilation*")
-    (error "not compilation buffer, won't auto-close")
-    )
-
-  (setq compilation-total-time (time-subtract nil compilation-start-time))
-  (setq time-str (concat " (Time: " (format-time-string "%s.%3N" compilation-total-time) "s)"))
-
-  (if
-      (with-current-buffer buffer
-        (setq warnings (eval compilation-num-warnings-found))
-        (setq warnings-str (concat " (Warnings: " (number-to-string warnings) ")"))
-        ;; (setq errors (eval compilation-num-errors-found))
-	(setq errors my/compilation-exit-code)
-
-        (if (eq errors 0) nil t)
-	)
-
-      ;;If Errors then
-      (message (concat "Compiled with Errors" " (exit code: " (number-to-string my/compilation-exit-code) ") " time-str))
-
-    ;;If Compiled Successfully or with Warnings then
-    (progn
-      (bury-buffer buffer)
-      (run-with-timer my/auto-hide-compile-buffer-delay nil 'delete-window (get-buffer-window buffer 'visible))
-      (message (concat "Compiled Successfully" warnings-str time-str))
-      )
-    )
-  )
-
 (make-variable-buffer-local 'compilation-start-time)
+(make-variable-buffer-local 'compilation-exit-msg)
 
 (defun my/compilation-started (proc)
-  (setq my/compilation-exit-code "0")
   (setq compilation-start-time (current-time))
+  (message "Compilation started")
   )
 
-(setq compilation-exit-message-function 'my/compilation-exit-message-function)
+(defun my/compilation-finished (buf msg)
+  (message (concat "Compilation: " compilation-exit-msg) "")
+  )
+
 (add-hook 'compilation-start-hook 'my/compilation-started)
-(add-hook 'compilation-finish-functions 'my/hide-compile-buffer-if-successful)
+(add-hook 'compilation-finish-functions 'my/compilation-finished)
+
+;; https://stackoverflow.com/questions/17659212/dont-display-compilation-buffer-in-emacs-until-the-process-exits-with-error-o
+;; Prevent the compilation buffer from being displayed until the compilation
+;; process has finished. After that, ensure that this buffer is visible if and
+;; only if the compilation failed.
+
+;; (compile.el displays the buffer with (allow-no-window . t))
+(add-to-list 'display-buffer-alist
+             '("\\*compilation\\*" (display-buffer-no-window)))
+;; After the compilation process is finished:
+(setq compilation-exit-message-function
+      (lambda (status code msg)
+	(setq compilation-total-time (time-subtract nil compilation-start-time))
+	(setq time-str (concat "(Time: " (format-time-string "%s.%3N" compilation-total-time) "s)"))
+
+        (let ((compilation-window
+               (get-buffer-window "*compilation*")))
+          (cond
+           ;; If compilation failed and compilation buffer is not visible,
+           ((and (eq status 'exit)
+                 (not (zerop code))
+                 (not compilation-window))
+            ;; temporarily allow displaying the buffer,
+            (let ((display-buffer-overriding-action
+                   '((display-buffer-use-least-recent-window))))
+	      (setq compilation-exit-msg (concat "FAILED" " (exit code: " (number-to-string code) ") " time-str))
+              ;; and display that buffer.
+              (display-buffer "*compilation*")))
+
+           ;; If compilation succeeded and compilation buffer is visible,
+           ((and (eq status 'exit)
+                 (zerop code))
+	    (setq compilation-exit-msg (concat "PASSED " time-str))
+	    ;; is it visible
+	    (if compilation-window
+		(delete-window compilation-window)
+	      ))
+	   )
+	  )
+        (cons msg code))) ;; default return value
