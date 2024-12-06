4865cdc423fd64728530b569b4d147579b0b6498
Pushpal Sidhu <psidhu.devel@gmail.com>
2024-12-06 11:03:56 -0800

    lisp: sh-mode: update style
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>

--- lisp/0032-sh-mode.el
+++ lisp/0032-sh-mode.el
@@ -5,6 +5,14 @@
 ;; Date: 20171130                                                            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Change style
+(setq sh-basic-offset 2)
+
+;; No tabs in sh-mode
+(add-hook 'sh-mode-hook
+	  (lambda ()
+	    (setq indent-tabs-mode nil)))
+
 ;; Turn on sh-mode with certain file extensions
 (util/hook-ext-to-mode "bash" 'sh-mode)		; bash
 (util/hook-ext-to-mode "bash_aliases" 'sh-mode)	; bash
